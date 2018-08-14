473872fd9b4ab330cdbaf3dfd21fff3f62d144d0
deymusis <vulpinari@yandex.ru>
2018-08-14 16:26:02 +0300

    Форма входа тепрь прилипает как надо.

--- js/common.js
+++ js/common.js
@@ -1,21 +1,34 @@
-// $(function(){
-// $(".carousel").jCarouselLite({
-// 		btnNext: ".next",
-// 		btnPrev: ".prev"
-// 	});
-// });
+function HeadTop (){ 
+    var $toppanel = $("#TopPanel"); //Получаем нужный объект var
+    var topOftoppanel = $toppanel.offset().top; //Получаем начальное расположение нашего блока 
+    $(window).scroll(function () {
+        var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно 
+        
+        if (windowScroll > topOftoppanel){ // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+            $toppanel.removeClass("subs").addClass("topWindow");
+            
+        } else {
+            $toppanel.removeClass("topWindow").addClass("subs");
+        }; 
+        console.log("windowScroll " + windowScroll + ", topOftoppanel" + topOftoppanel); 
+    });
+};
 
 
+function init(){
+    $(".externalControl .carousel").jCarouselLite({
+        visible: 1,
+        start: 0,
+        btnNext: ".externalControl .next",
+        btnPrev: ".externalControl .prev",
+        btnGo:
+        [".externalControl .one", ".externalControl .two",
+        ".externalControl .three",".externalControl .four",
+        ".externalControl .five",".externalControl .six"]
+    });
 
-$(function(){
-$(".externalControl .carousel").jCarouselLite({
-    visible: 1,
-    start: 0,
-    btnNext: ".externalControl .next",
-    btnPrev: ".externalControl .prev",
-    btnGo:
-    [".externalControl .one", ".externalControl .two",
-    ".externalControl .three",".externalControl .four",
-    ".externalControl .five",".externalControl .six"]
-});
-});
+    HeadTop();
+}
+
+$(document).ready(init);
+
