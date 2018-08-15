001118fc4573873741f957674e8dc1b936fe3612
deymusis <vulpinari@yandex.ru>
2018-08-15 15:41:19 +0300

    Добавил адаптивность.

--- js/common.js
+++ js/common.js
@@ -1,34 +1,41 @@
-function HeadTop (){ 
-    var $toppanel = $("#TopPanel"); //Получаем нужный объект var
-    var topOftoppanel = $toppanel.offset().top; //Получаем начальное расположение нашего блока 
-    $(window).scroll(function () {
-        var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно 
-        
-        if (windowScroll > topOftoppanel){ // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
-            $toppanel.removeClass("subs").addClass("topWindow");
+
+var page = (function(){
+    function init() {
+        initCarousel();
+        headTop();
+    }
+    
+    function initCarousel(){
+        $(".externalControl .carousel").jCarouselLite({
+            visible: 1,
+            start: 0,
+            btnNext: ".externalControl .next",
+            btnPrev: ".externalControl .prev",
+            btnGo:
+            [".externalControl .one", ".externalControl .two",
+            ".externalControl .three",".externalControl .four",
+            ".externalControl .five",".externalControl .six"]
+        });
+    }
+    
+    function headTop (){ 
+        var $toppanel = $("#topPanel"); //Получаем нужный объект var
+        var topOfToppanel = $toppanel.offset().top; //Получаем начальное расположение нашего блока 
+        $(window).scroll(function () {
+            var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно 
             
-        } else {
-            $toppanel.removeClass("topWindow").addClass("subs");
-        }; 
-        console.log("windowScroll " + windowScroll + ", topOftoppanel" + topOftoppanel); 
-    });
-};
+            if (windowScroll > topOfToppanel){ // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+                $toppanel.removeClass("subs").addClass("topWindow");
+                
+            } else {
+                $toppanel.removeClass("topWindow").addClass("subs");
+            }; 
+            console.log("windowScroll " + windowScroll + ", topOfToppanel" + topOfToppanel); 
+        });
+    };
+    return {
+        init: init
+    };
+}());
 
-
-function init(){
-    $(".externalControl .carousel").jCarouselLite({
-        visible: 1,
-        start: 0,
-        btnNext: ".externalControl .next",
-        btnPrev: ".externalControl .prev",
-        btnGo:
-        [".externalControl .one", ".externalControl .two",
-        ".externalControl .three",".externalControl .four",
-        ".externalControl .five",".externalControl .six"]
-    });
-
-    HeadTop();
-}
-
-$(document).ready(init);
-
+$(document).ready(page.init);
