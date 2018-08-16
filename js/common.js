31d393155adfde6b43180eb26b610011b108efcb
deymusis <vulpinari@yandex.ru>
2018-08-16 15:13:12 +0300

    Галочки заработали.

--- js/common.js
+++ js/common.js
@@ -38,3 +38,45 @@ var page = (function(){
 }());
 
 $(document).ready(page.init);
+
+
+var checkbox = (function() {
+    var checkboxes = [
+        {id: "subscribe", state:0},
+        {id: "agree", state:0, check:true}
+    ],
+    checkClass = "checked";
+
+    var check = function() {
+        var $self = $(this);
+        var id = $self.attr("id");
+        var check = checkboxes.find(function(item){
+            return item.id === id
+        });
+        var $child = $self.children("div");
+        
+        if(check.state === 1){
+            $child.removeClass(checkClass);
+            check.state = 0;
+            if (check.check){
+                $("#btnEditing").addClass("colorOrange");
+            }
+        } else {
+            $child.addClass(checkClass);
+            check.state = 1;
+            if (check.check){
+                $("#btnEditing").removeClass("colorOrange");
+            }
+        }
+    }
+
+    var checkboxs = function(){
+        checkboxes.forEach(function(element) {
+            $("#" + element.id).click(check);
+        });
+    }
+    return{
+        cb: checkboxs
+    };
+})();
+checkbox.cb();
