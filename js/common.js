2eba22b4de0a33261b6b40f3083b2bc5a47b52b6
deymusis <vulpinari@yandex.ru>
2018-08-17 18:59:00 +0300

    1. Добавил стрелки. 2. Пофиксил точки-кнопки.

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-
 var page = (function(){
     function init() {
         initCarousel();
@@ -8,8 +7,8 @@ var page = (function(){
     function initCarousel(){
         $(".externalControl .carousel").jCarouselLite({
             visible: 1,
-            btnNext: ".externalControl .next",
-            btnPrev: ".externalControl .prev",
+            btnNext: ".externalControl .slick-next",
+            btnPrev: ".externalControl .slick-prev",
             btnGo:
             [".externalControl .one", ".externalControl .two",
             ".externalControl .three",".externalControl .four",
@@ -31,6 +30,33 @@ var page = (function(){
             }; 
             console.log("windowScroll " + windowScroll + ", topOfToppanel" + topOfToppanel); 
         });
+
+        $('#subscribe').click(toggleChecked);
+
+        $('#agree').click(function() {
+            toggleChecked.call(this);
+            disableButton();
+        });
+    
+        function toggleChecked() {
+            var $check = $(this).children('div');
+    
+            if ($check.hasClass("checked")) {
+                $check.removeClass("checked")
+            } else {
+                $check.addClass("checked")
+            }
+        }
+    
+        function disableButton() {
+            var $chk = $('.btn-start');
+    
+            if ($chk.hasClass("colorOrange")) {
+                $chk.removeClass("colorOrange")
+            } else {
+                $chk.addClass("colorOrange")
+            }
+        }
     };
     return {
         init: init
@@ -39,92 +65,3 @@ var page = (function(){
 
 $(document).ready(page.init);
 
-
-// var checkbox = (function() {
-//     var checkboxes = [
-//         {id: "subscribe", state:0},
-//         {id: "agree", state:0, check:true}
-//     ],
-//     checkClass = "checked";
-
-//     var check = function() {
-//         var $self = $(this);
-//         var id = $self.attr("id");
-//         var check = checkboxes.find(function(item){
-//             return item.id === id
-//         });
-//         var $child = $self.children("div");
-        
-//         if(check.state === 1){
-//             $child.removeClass(checkClass);
-//             check.state = 0;
-//             if (check.check){
-//                 $("#btnEditing").addClass("colorOrange");
-//             }
-//         } else {
-//             $child.addClass(checkClass);
-//             check.state = 1;
-//             if (check.check){
-//                 $("#btnEditing").removeClass("colorOrange");
-//             }
-//         }
-//     }
-
-//     var checkboxs = function(){
-//         checkboxes.forEach(function(element) {
-//             $("#" + element.id).click(check);
-//         });
-//     }
-//     return{
-//         cb: checkboxs
-//     };
-// })();
-// checkbox.cb();
-
-
-
-
-$(document).ready(function() {
-
-    $('#subscribe').click(toggleChecked);
-
-    $('#agree').click(function() {
-        toggleChecked.call(this);
-        disableButton();
-    });
-
-    function toggleChecked() {
-        var $check = $(this).children('div');
-
-        if ($check.hasClass("checked")) {
-            $check.removeClass("checked")
-        } else {
-            $check.addClass("checked")
-        }
-    }
-
-    function disableButton() {
-        var $chk = $('.btn-start');
-
-        if ($chk.hasClass("colorOrange")) {
-            $chk.removeClass("colorOrange")
-        } else {
-            $chk.addClass("colorOrange")
-        }
-    }
-
-});
-
-
-
-
-// $(document).ready(function() {
-
-//     $('#agree').click(function() {
-//         var $check = $('#agree div');
-//         $check.addClass('checked');
-
-    
-
-// });
-// });
